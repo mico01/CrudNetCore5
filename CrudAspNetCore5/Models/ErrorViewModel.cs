a2e63c13afc66adc1b4aaf29bfd5722c42250d6f
mico01 <54836074+mico01@users.noreply.github.com>
2021-04-19 20:17:13 +0200

    start project

---
+++ CrudAspNetCore5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CrudAspNetCore5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
